797be0ff0b11dab4c901b67879eaddcb27673937
Maria Kennedy <mariakingsbury1@gmail.com>
2024-04-27 21:52:39 -0500

    installed bootstrap, added nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :add_ons
   resources :meals
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "meals#index"
+  root "home#index"
   # get "home", to: "home#index"
 end
